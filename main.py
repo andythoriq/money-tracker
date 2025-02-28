69065e80d79a3c668a1196590f1039d73ffb1e08
andythoriq <andyythoriqq@gmail.com>
2025-02-28 20:21:53 +0700

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 import sys
-from pages.view_income import IncomeWindow
+from pages.income.view_income import IncomeWindow
 
 class Dashboard(QWidget):
     def __init__(self):
@@ -16,15 +16,33 @@ class Dashboard(QWidget):
         self.btn_income = QPushButton("Tambah Income", self)
         self.btn_income.clicked.connect(self.open_income)
 
+        self.btn_outcome = QPushButton("Tambah Outcome", self)
+        # self.btn_outcome.clicked.connect()
+
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.btn_income)
+        self.layout.addWidget(self.btn_outcome)
 
         self.setLayout(self.layout)
 
+    # method untuk masing-masing window
     def open_income(self):
         self.income_window = IncomeWindow()
         self.income_window.show()
+
+    def open_outcome(self):
+        pass
+
+    def open_wallet(self):
+        pass
+
+    def open_wishlist(self):
+        pass
+
+    def open_category(self):
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
