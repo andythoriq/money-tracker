8d98abb2036d7b052e2148bbed2694930d72b661
Zaidan <zaidanarkaan1@gmail.com>
2025-03-03 22:43:13 +0700

    Debug

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ if __name__ == '__main__':
 
     load_stylesheet(app)
 
-    # Create and show splash screen
     splash = SplashScreen()
     splash.show()
 
-    # Load main window after delay
     def start_main_window():
-        splash.close()  # Close splash screen
+        splash.close()  
         dashboard = Dashboard()
         dashboard.show()
 
-    # Set delay before opening main window (e.g., 3 seconds)
     QTimer.singleShot(3000, start_main_window)
 
     sys.exit(app.exec_())
