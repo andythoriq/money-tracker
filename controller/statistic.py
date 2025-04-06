ba13febb7b5851bb419ef67658dbf0fa5b3e1339
Faris241524010 <farisichsanalyawa@gmail.com>
2025-04-07 06:57:35 +0700

    adjust

--- controller/statistic.py
+++ controller/statistic.py
@@ -11,9 +11,7 @@ class Statistic:
         super().__init__()
         self.income_controller = Income(Wallet())
         self.outcome_controller = Outcome(Wallet())
-        self.offset = 0
-        self.jenis = 0
-        self.cur_data = self.generate_data(self.offset)
+        self.cur_data = self.generate_data()
 
     def generate_data(self, offset = 0, jenis = "harian"):
         # Ambil dan konversi data dari Outcome
@@ -26,7 +24,7 @@ class Statistic:
                 outcome_date.append(outcome[5])
         else:
                 outcome_amount.append(0)
-                outcome_date.append("1/1/1900")
+                outcome_date.append("01/01/1900")
 
         # Ambil dan konversi data dari Income
         income_amount = []
@@ -38,7 +36,7 @@ class Statistic:
                 income_date.append(income[5])
         else:
                 income_amount.append(0)
-                income_date.append("1/1/1900")
+                income_date.append("01/01/1900")
 
 
         # Menggabungkan kedua list dan menyortir berdasarkan tanggal
@@ -55,14 +53,16 @@ class Statistic:
         sorted_tanggalO = list(sorted_tanggalO)
         sorted_amountO = list(sorted_amountO)
 
+        x = []
         yI = []
         yO = []
+        labels = []
         incomeSum = 0
         outcomeSum = 0
         temp = 0
 
         if (jenis == "harian"):
-            for x in self.ubahHari(self.offset):
+            for x in self.ubahHari(offset):
                 try:
                     indeks = sorted_tanggalI.index(x)
                     temp += sorted_amountI[indeks]
@@ -79,7 +79,7 @@ class Statistic:
                 except ValueError:
                     yI.append(0)
 
-            for x in self.ubahHari(self.offset):
+            for x in self.ubahHari(offset):
                 try:
                     indeks = sorted_tanggalO.index(x)
                     temp += sorted_amountO[indeks]
@@ -97,12 +97,15 @@ class Statistic:
                     yO.append(0)
 
             # Data untuk x axis harian
-            time = self.ubahHari(self.offset) # nama hari
+            time = self.ubahHari(offset) # nama hari
+            x = np.arange(len(time))
             description = self.NamaHariDariTanggal(time) # tanggal hari
             labels = [f"{description[i]}\n({time[i]})" for i in range(len(time))]
 
         if (jenis == "mingguan"):
-            for x1 in self.BulanSkrngMingguan(self.offset):
+            BSM = self.BulanSkrngMingguan(offset)
+                        
+            for x1 in self.BulanSkrngMingguan(offset):
                 for x2 in x1:
                     try:
                         indeks = sorted_tanggalI.index(x2)
@@ -122,7 +125,7 @@ class Statistic:
                 yI.append(temp)
                 temp = 0
 
-            for x1 in self.BulanSkrngMingguan(self.offset):
+            for x1 in self.BulanSkrngMingguan(offset):
                 for x2 in x1:
                     try:
                         indeks = sorted_tanggalO.index(x2)
@@ -143,9 +146,10 @@ class Statistic:
                 temp = 0
             
             # Data untuk x axis harian
-            time = self.BulanSkrngMingguan(self.offset) # nama hari
-            for x in range(len(time)):
-                time[x] = time[x][0]
+            time = self.BulanSkrngMingguan(offset) # nama hari
+            x = np.arange(len(time))
+            for i in range(len(time)):
+                time[i] = time[i][0]
             description = ["1st Week", "2nd Week", "3rd Week", "4th Week"] # tanggal hari
             if len(time) == 5:
                 description.append("5th Week")
@@ -156,12 +160,11 @@ class Statistic:
             # Konversi sorted_tanggalI menjadi (bulan, tahun)
             tanggal_list = [(int(t.split('/')[1]), int(t.split('/')[2])) for t in sorted_tanggalI]
             # Iterasi melalui bulan dan tahun dari BulanSkrng
-            for bulan, tahun in zip(*self.BulanSkrng(self.offset)):
+            for bulan, tahun in zip(*self.BulanSkrng(offset)):
                 temp = 0  # Menyimpan total income per bulan
 
 		        # Iterasi semua tanggal dan mencari yang sesuai
                 for i, (b, t) in enumerate(tanggal_list):
-                    print(self.BulanSkrng(self.offset))
                     if (b, t) == (bulan, tahun):  # Jika bulan & tahun cocok
                         temp += sorted_amountI[i]
 				
@@ -171,7 +174,7 @@ class Statistic:
 
             tanggal_list = [(int(t.split('/')[1]), int(t.split('/')[2])) for t in sorted_tanggalO]
             # Iterasi melalui bulan dan tahun dari BulanSkrng
-            for bulan, tahun in zip(*self.BulanSkrng(self.offset)):
+            for bulan, tahun in zip(*self.BulanSkrng(offset)):
                 temp = 0  # Menyimpan total income per bulan
         
                 # Iterasi semua tanggal dan mencari yang sesuai
@@ -184,18 +187,17 @@ class Statistic:
                 outcomeSum += temp
 
             # Data untuk x axis harian
-            blnThn = self.BulanSkrng(self.offset) # nama hari
+            blnThn = self.BulanSkrng(offset) # nama hari
             time = blnThn[0]
+            x = np.arange(len(time))
             description = self.NamaBulanDariAngka(time) # tanggal hari
             labels = [f"{description[i]}\n({blnThn[1][i]})" for i in range(len(time))]
-            print(yI)
-            print(yO)
         
         # Data untuk x axis tahunan
         if jenis == "tahunan":
             tanggal_list = [int(t.split('/')[2]) for t in sorted_tanggalI]
 			
-            for tahun in self.TahunSkrng(self.offset):
+            for tahun in self.TahunSkrng(offset):
                 temp = 0  
             
                 for i, t in enumerate(tanggal_list):
@@ -207,7 +209,7 @@ class Statistic:
 			
             tanggal_list = [int(t.split('/')[2]) for t in sorted_tanggalO]
 			
-            for tahun in self.TahunSkrng(self.offset):
+            for tahun in self.TahunSkrng(offset):
                 temp = 0  
 			    
                 for i, t in enumerate(tanggal_list):
@@ -217,13 +219,11 @@ class Statistic:
 			    # Simpan hasil per tahun dan tambahkan ke total
                 yO.append(temp)
                 outcomeSum += temp
-            time = self.TahunSkrng(self.offset)
+            time = self.TahunSkrng(offset)
+            x = np.arange(len(time))
             labels = [f"{time[i]}" for i in range(len(time))]
-
-        # Menggunakan np.arange untuk membuat sumbu x
-        x = np.arange(len(time))  # Akan menghasilkan 7 hari
         
-        return x, yI, yO, labels
+        return x, yI, yO, labels, incomeSum, outcomeSum
 
     # Fungsi untuk menyimpan grafik
     def save_graph(self):
@@ -254,17 +254,17 @@ class Statistic:
     
     def ubahHari(self, offset=0):
         today = datetime.today()
-        start_of_week = today + timedelta(days=offset)
+        start_of_week = today + timedelta(days=offset * 7)
         return [
             (start_of_week + timedelta(days=i)).strftime("%d/%m/%Y") 
             for i in range(7)
         ]
-
+    
     def BulanSkrngMingguan(self, offset=0):  # Menambahkan parameter offset (default 0)
         today = datetime.today()  # Mendapatkan tanggal hari ini
         
         # Menghitung bulan dan tahun baru berdasarkan offset
-        new_month = today.month + offset
+        new_month = today.month + offset * 6
         new_year = today.year
         
         # Jika bulan baru melebihi Desember, sesuaikan tahun
@@ -352,11 +352,64 @@ class Statistic:
             years.append(start_year + i + offset)
         
         return years
-    
-    def change_offset(self, direction):
-        if direction == 'next':
-            self.offset += 1
-        elif direction == 'prev':
-            self.offset -= 1
-        self.cur_data = self.generate_data(self.offset)
-        self.generate_statistics()
+
+    def generate_statistics(self):
+        self.plot_widget.clear()
+
+        data = self.cur_data
+
+        x = data[0]
+        income_barItem = data[1]
+        outcome_barItem = data[2]
+        infoLabels = data[3]
+        maxNilai = max(data[1] + data[2])
+
+        # Membuat grafik batang untuk pendapatan (warna hijau) dan pengeluaran (warna merah)
+        income_graph = BarGraphItem(x=x-0.15, height=income_barItem, width=0.3, brush='#FF9F40', name="Pendapatan")
+        outcome_graph = BarGraphItem(x=x+0.15, height=outcome_barItem, width=0.3, brush='#F4BE37', name="Pengeluaran")
+
+        # Menambahkan grafik batang ke plot
+        self.plot_widget.addItem(income_graph)
+        self.plot_widget.addItem(outcome_graph)
+        # Mengubah ticks sumbu X untuk menampilkan dua informasi: hari dan tanggal
+        self.plot_widget.getAxis('bottom').setTicks([list(zip(x, infoLabels))])
+
+        # Mengubah sumbu Y untuk lebih baik menampilkan data
+        self.plot_widget.setLabel('left', 'Nilai', units="")
+        self.plot_widget.setLabel('bottom', 'Hari dan Tanggal')
+
+        # Display values on top of bars (income bars)
+        for i, val in enumerate(income_barItem):
+            text = TextItem(str(val), color='green')
+            text.setPos(x[i] - 0.25, val + 3)  # Position above the bar
+            self.plot_widget.addItem(text)
+
+        # Display values on top of bars (outcome bars)
+        for i, val in enumerate(outcome_barItem):
+            text = TextItem(str(val), color='red')
+            text.setPos(x[i], val + 3)  # Position above the bar
+            self.plot_widget.addItem(text)
+
+        # Menyesuaikan format label sumbu Y untuk menghindari notasi ilmiah
+        axis = self.plot_widget.getAxis('left')
+
+        # Periksa apakah maxNilai kurang dari 5 dan tentukan langkah minimal
+        step = max(1, int(maxNilai / 5))  # Pastikan langkah tidak menjadi 0
+
+        # Membuat ticks dengan langkah yang aman
+        ticks = [(i, "{:,.0f}".format(i)) for i in range(0, maxNilai + 1, step)]
+
+        # Menetapkan ticks pada axis
+        axis.setTicks([ticks])
+
+        # Tombol untuk mengubah view range setelah beberapa detik
+        self.plot_widget.getPlotItem().vb.setRange(yRange=(0, int(maxNilai * 1.3)), padding=0)
+
+        # Menghilangkan toolbar atau elemen lainnya
+        self.plot_widget.getPlotItem().hideButtons()  # Menghilangkan tombol atau kontrol lainnya
+
+        # Menambahkan grid pada plot untuk mempermudah pembacaan
+        self.plot_widget.showGrid(x=True, y=True, alpha=0.3)  # Alpha untuk transparansi grid
+
+        # Menambahkan legenda ke plot
+        self.plot_widget.addLegend()  # Ini adalah cara yang benar untuk menambahkan legenda
